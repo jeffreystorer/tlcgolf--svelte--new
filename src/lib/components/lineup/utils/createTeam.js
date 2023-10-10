e39390f0194956d1f7c8aed3ee010466fed18c9e
jeffreystorer <jeffrey.storer@gmail.com>
2023-10-10 12:34:29 -0400

    10/10: Before Lunch (2)

--- src/lib/components/lineup/utils/createTeam.js
+++ src/lib/components/lineup/utils/createTeam.js
@@ -1,9 +1,10 @@
+import * as _ from 'lodash';
 import { get } from 'svelte/store';
 import {playersInLineup, teamTables} from '$lib/store';
 
 export default function createTeam(autoPop) {
   let _playersInLineup = get(playersInLineup);
-  let _teamTables = get(teamTables);
+  let _teamTables = _.cloneDeep(get(teamTables));
   for (let i = 0; i < autoPop.length; i++) {
     for (let j = 0; j < autoPop[i].length; j++) {
       let newPlayerObj = _playersInLineup[autoPop[i][j]];
