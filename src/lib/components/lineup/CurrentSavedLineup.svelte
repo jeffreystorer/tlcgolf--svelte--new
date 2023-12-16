e309aa455d5413e486951a41e5fed08a436117b4
jeffreystorer <jeffrey.storer@gmail.com>
2023-12-16 15:55:03 -0500

    12/16: Clear teamTables, etc. on Clear

--- src/lib/components/lineup/CurrentSavedLineup.svelte
+++ src/lib/components/lineup/CurrentSavedLineup.svelte
@@ -2,7 +2,7 @@
   export let lineupSnapshot;
   import { goto } from '$app/navigation';
   import {onMount}  from 'svelte';
-  import {playersInLineup,currentLineupIndex,currentLineup,lineupTitle,sortOrder,playingDate,teeTimeCount,linkTime,progs069,progAdj,currentLineupKey,deleteAll} from '$lib/store';
+  import {playersInLineup,currentLineupIndex,currentLineup,lineupTitle,sortOrder,playingDate,teeTimeCount,linkTime,progs069,progAdj,currentLineupKey,deleteAll, teamTables, showAddTeamMember} from '$lib/store';
   import { ConfirmDeleteModal } from '$lib/components/lineup';
   import { loadSavedLineup } from '$lib/components/lineup/utils';
 
@@ -41,6 +41,31 @@
     $linkTime = 'Set Link Time Above';
     $progs069 = '';
     $progAdj = '';
+    $teamTables = {
+      teeAssignments: [1],
+      team0: [],
+      team1: [],
+      team2: [],
+      team3: [],
+      team4: [],
+      team5: [],
+      team6: [],
+      team7: [],
+      team8: [],
+      team9: []
+    };
+    $showAddTeamMember = {
+      team0: false,
+      team1: false,
+      team2: false,
+      team3: false,
+      team4: false,
+      team5: false,
+      team6: false,
+      team7: false,
+      team8: false,
+      team9: false
+    };
   };
 
   function handleDelete(e) {
